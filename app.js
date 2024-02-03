476f9a5a25e649a22d1de799e5532f119f1f6f1e
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-02-03 13:09:06 +0530

    delete review feature added

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.delete("/listings/:id", wrapAsync( async (req,res)=>{
 }));
 
 // Reviews
-// Post Route
+// Post Reviews Route
 app.post("/listings/:id/reviews", validateReview, wrapAsync( async (req,res) =>{
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
@@ -126,6 +126,15 @@ app.post("/listings/:id/reviews", validateReview, wrapAsync( async (req,res) =>{
     await listing.save();
 
     res.redirect(`/listings/${listing._id}`);
+}));
+
+// Delete Reviews Route
+app.delete("/listings/:id/reviewss/:reviewId", wrapAsync( async (req,res)=>{
+    let {id,reviewId} = req.params;
+
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
 }));
 
 // app.get("/testListing",async (req,res) =>{
