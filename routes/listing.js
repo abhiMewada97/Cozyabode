31c9b2d07f5bd8952a6dc5406f0b660cb3c41372
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-02-16 21:30:40 +0530

    use controllers of MVC for routing

--- routes/listing.js
+++ routes/listing.js
@@ -4,90 +4,31 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middelware.js");
 
+const listingController = require("../controllers/listings.js");        // using MVC framwork
+
 // Index Route
-router.get("/", wrapAsync( async (req,res) =>{     //replace all app. --by-- router. // replace all "/listings" --by-- "/"
-    let allListings = await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
+router.get("/", wrapAsync( listingController.index));     //replace all app. --by-- router. // replace all "/listings" --by-- "/"
 
 // New Route
-router.get("/new", isLoggedIn, (req,res)=> {
-    res.render("listings/new.ejs");
-});
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 // Show Route
-router.get("/:id", wrapAsync( async (req,res) =>{
-    let {id} = req.params;
-
-    // const listing = await Listing.findById(id)                // here we are populating listing
-    //                             .populate("reviews")          // listing ko populate karne ke sath sath authour ko bhi populate karege for each review -> for that we use nested populate
-    //                             .populate("owner");           // nested populate means populating all reviews and their owner
-    
-    const listing = await Listing.findById(id)
-                                .populate({
-                                    path: "reviews",
-                                    populate: {
-                                        path: "auther",
-                                    },            // here we populate authore with reviews // so for that we pass opbect in first populate and first parameter is "path"
-                                })                // path me all review will come // and har ek review ke liye path me author aa jaye
-                                .populate("owner");
-    if(!listing) {
-        req.flash("error","Listing you requested for, does not exit");
-        res.redirect("/listings");
-    }
-    console.log(listing);
-    res.render("listings/show.ejs",{listing});
-}));
+router.get("/:id", wrapAsync( listingController.showListing ));
 
 // Create Route
-router.post("/", isLoggedIn,
-    validateListing, 
-    wrapAsync(async (req,res,next) =>{
-
-        // let {title,description,price,location,country} = req.body;
-        // let listing = req.body.listing;
-
-        const newListing = new Listing(req.body.listing);
-        console.log(req.user);
-        newListing.owner = req.user._id;     // we are saving information of current user inside,  owner inside newListing 
-                                            // how to save info --> we know req object ke ander passport by default user related information store karta hai inside req.user and it has many diff diff value
-        await newListing.save();
-        req.flash("success", "New Listing Created!");
-        res.redirect("/listings");
-    })
+router.post("/", isLoggedIn, validateListing, wrapAsync(listingController.createListing)
 );
 
 // Edit Route
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync( async (req,res) =>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    if(!listing) {
-        req.flash("error","Listing you requested for, does not exit");
-        res.redirect("/listings");
-    }
-    res.render("listings/edit.ejs",{listing});
-}));
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync( listingController.renderEditForm));
 
 // Update Route
 router.put("/:id", isLoggedIn, 
     isOwner,
     validateListing,
-    wrapAsync( async (req,res) =>{
-
-    let {id} = req.params;
-
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});  // {...req.body.listing} js object which have all parameter and convert it into seperated value
-    req.flash("success", "Listing Updated!");
-    res.redirect(`/listings/${id}`);
-}));
+    wrapAsync( listingController.upddateListing));
 
 // Delete Route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync( async (req,res)=>{
-    let {id} = req.params;
-    let deleteListing = await Listing.findByIdAndDelete(id);  // when findByIdAndDelete call, so as a middleware listingSchema.post will inside listing.js
-    console.log(deleteListing);
-    req.flash("success", "Listing Deleted!");
-    res.redirect("/listings");
-}));
+router.delete("/:id", isLoggedIn, isOwner, wrapAsync( listingController.destroyListing ));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,58 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
+
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middelware.js");
 
+const userController = require("../controllers/users.js");
 
 // Sign Up user
-router.get("/signup", (req,res) => {
-    res.render("users/signup.ejs");
-});
+router.get("/signup", userController.renderSignupForm);
 
-router.post("/signup", wrapAsync(async(req,res) => {
-    try {
-        let { username, email, password } = req.body;
-        const newUser = new User({username, email});
-        const registerdUser = await User.register(newUser, password);
-        console.log(registerdUser);
-        req.login(registerdUser, (err) =>{
-            if(err) {
-                return next(err);
-            }
-            req.flash("success", "Welcome to Wanderlust!");
-            res.redirect("/listings");
-        })
-    } catch (err) {
-        req.flash("error",err.message);
-        res.redirect("/signup");
-    }
-}));
+router.post("/signup", wrapAsync( userController.signup ));
 
 // Login user
-router.get("/login", (req,res) => {
-    res.render("users/login.ejs");
-});
+router.get("/login", userController.renderLoginForm);
 
-// router.post("/login", passport.authenticate("local", {failureRedirect: '/login'}), async(req,res) => {    // stratigy-> "local"  // passport.authenticate() middleware use to authentaicate
-router.post("/login", saveRedirectUrl, passport.authenticate("local", {failureRedirect: '/login', failureFlash: true }), async(req,res) => { 
-        req.flash("success", "Welcome to Wanderlust! You are logged in!");
-        // res.redirect("/listings");
-        // res.redirect(req.session.redirectUrl);      // pasport by default reset req.session  // when login, login delete redirectUrl from req.session 
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);                    // pasport by default reset req.session  // when login, login delete redirectUrl from req.session 
-});
+router.post("/login", saveRedirectUrl, passport.authenticate("local", {failureRedirect: '/login', failureFlash: true }), );    // stratigy-> "local"  // passport.authenticate() middleware use to authentaicate
 
 // Logout User
-router.get("/logout", (req,res) =>{
-    req.logout((err) => {
-        if(err) {
-            return next(err);
-        }
-        req.flash("success","You are logged out!");
-        res.redirect("/listings");
-    });
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
