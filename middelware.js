9a18bcd1467e4e2c644e38787e6155c87bf5d5b3
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-03-08 13:18:46 +0530

    Admin functionality added

--- middelware.js
+++ middelware.js
@@ -24,7 +24,7 @@ module.exports.isOwner = async (req,res,next) => {
     
     let {id} = req.params;
     let listing = await Listing.findById(id);
-    if(!listing.owner.equals(res.locals.currUser._id)) {
+    if(!(listing.owner.equals(res.locals.currUser._id) || res.locals.currUser._id.equals('65e9c80af5221cdf1beb6b73'))) {
         req.flash("error", "You are not the owner of this listing");
         return res.redirect(`/listings/${id}`);
     }
@@ -57,7 +57,7 @@ module.exports.isReviewAuthor = async (req,res,next) => {
     
     let { id, reviewId } = req.params;
     let review = await Review.findById(reviewId);
-    if(!review.auther.equals(res.locals.currUser._id)) {
+    if(!(review.auther.equals(res.locals.currUser._id) || res.locals.currUser._id.equals('65e9c80af5221cdf1beb6b73'))) {
         req.flash("error", "You are not the owner of this review");
         return res.redirect(`/listings/${id}`);
     }
